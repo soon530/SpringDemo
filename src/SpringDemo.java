194614ca762e8270e1e24d4012104b8233e03dee
soon <soon530@gmail.com>
2013-07-27 16:06:26 +0800

    建立新的SpringDemo範例

---
+++ src/SpringDemo.java
@@ -0,0 +1,14 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class SpringDemo {
+	public static void main(String[] args) {
+		Resource rs = new FileSystemResource("beans-config.xml");
+		BeanFactory factory = new XmlBeanFactory(rs);
+
+		HelloBean hello = (HelloBean) factory.getBean("helloBean");
+		System.out.println(hello.getHelloWord());
+	}
+}
